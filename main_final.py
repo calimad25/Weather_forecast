919ac3226f63bb724512b3aa56fb1f7d4b9c0cb9
calimad <calimanescuadrian@gmail.com>
2023-01-15 20:28:48 +0200

    Add docstrings

--- main_final.py
+++ main_final.py
@@ -3,7 +3,28 @@ import pprint
 
 
 class Weather:
+    """Creates a weather object getting an apikey as input
+    and either a city name or lat and lon coordinates
 
+    Package use example
+
+    # Create a weather object using a city name:
+    # The apikey below is not guaranteed to work
+    # Get your own apikey from https://openwathermap.org
+    # Wait a couple of minutes for the apikey to be activated
+
+    >>> weather1 = Weather(apikey="8ac2f887096da01213fa3db827e593d4", city="Rome")
+
+    # Using latitude and longitude coordinates
+    >>> weather2 = Weather(apikey="8ac2f887096da01213fa3db827e593d4", lat=4.1, lon=4.5)
+
+    # Get complete weather data for the next 12 hours:
+    >>> weather1.next_12h()
+
+    # Simplified data for the next 12 hours:
+    >>> weather1.next_12h_simplified()
+
+    """
     def __init__(self, apikey, city=None, lat=None, lon=None):
         if city:
             url = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={apikey}&units=metric"
@@ -20,18 +41,15 @@ class Weather:
             raise ValueError(self.data["message"])
 
     def next_12h(self):
+        """Returns 3-hour data for the next 12 hours as a dict
+        """
         return self.data['list'][:4]
 
     def next_12h_simplified(self):
+        """Returns date and time, temperature and sky condition every 3 hours for the
+            next 12 hours as a tuple
+        """
         simple_data = []
         for i in self.data['list'][:4]:
             simple_data.append((i['dt_txt'], i['main']['temp'], i['weather'][0]['description']))
         return simple_data
-
-
-# weather = Weather(apikey="8ac2f887096da01213fa3db827e593d4", city="Rome")
-# weather = Weather(apikey="8ac2f887096da01213fa3db827e593d4", lat=4.1, lon=4.5)
-# weather = Weather(apikey="8ac2f887096da01213fa3db827e593d4")
-weather = Weather(apikey="8ac2f887096da01213fa3db827e593d4", city="asdasd")
-# print(weather.data)
-pprint.pprint(weather.next_12h_simplified())
