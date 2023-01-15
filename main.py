d139d4428a39fa8502e17d81572d797e6e8ef07e
calimad <calimanescuadrian@gmail.com>
2023-01-15 12:42:10 +0200

    Create url and test the empty methods

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ print(r.json())
 
 class Weather:
 
-    def __init__(self, apikey, city, lon, lat):
-        pass
+    def __init__(self, apikey, city, lon=None, lat=None):
+        url = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={apikey}&units=metric"
+        r = requests.get(url)
+        self.data = r.json()
 
     def next_12h(self):
-        pass
+        return self.data
 
     def next_12h_simplified(self):
         pass
 
+
+weather = Weather(apikey="8ac2f887096da01213fa3db827e593d4", city="Rome")
+print(weather.data)
+print(weather.next_12h())
